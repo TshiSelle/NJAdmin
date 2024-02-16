d9f9fac9a4aa3fd30ace9826cd91c16d7c787c18
Omar Houssami <72316065+TshiSelle@users.noreply.github.com>
2024-02-16 13:42:10 +0200

    added ping tool

---
+++ src/commands/tools/ping.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Replies with Pong!'),
+  async execute(interaction, client) {
+    const message = await interaction.deferReply({
+      fetchReply: true,
+    });
+
+    const newMessage = `API Latency: ${client.ws.ping}ms \n Client Ping: ${
+      message.createdTimestamp - interaction.createdTimestamp
+    }ms`;
+    await interaction.editReply({
+      content: newMessage,
+    });
+  },
+};
